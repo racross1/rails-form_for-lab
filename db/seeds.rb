55b31bc6867ad05e12dd6f08b37f3e95f65e058a
racross1 <racross1@gmail.com>
2020-12-14 17:26:45 -0800

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SchoolClass.destroy_all
+Student.destroy_all
+
+sc1 = SchoolClass.create(title: "classy class", room_number: 1)
+
+s1 = Student.create(first_name: "suzy", last_name: "sioux")
+
+puts "done"
